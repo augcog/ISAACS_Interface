294eb99a6f183294b6f185eef792c0ca67aaf1e1
Apollo <apollo@apollo.vision>
2020-06-11 17:16:45 -0700

    Added Origin and Velocities. Untested. Grabbing check and getter methods left.

--- Assets/Scripts/Interaction Scripts/ControllerState.cs
+++ Assets/Scripts/Interaction Scripts/ControllerState.cs
@@ -19,10 +19,11 @@
 		private bool leftX;
 		private bool leftY;
 		private bool leftThumb;
-		private float leftThumbDeltaX; // The distance by which the left stick has been moved in the X-axis.
-		private float leftThumbDeltaZ; // The distance by which the left stick has been moved in the Z-axis.
-		private Vector3 leftPosition; // The position of the left controller.
+		private Vector2 leftThumbDelta; // The distance by which the left stick has been moved in the X-axis and Y-axis.
+		private float leftThumbAngle; // The angle of rotation of the left stick.
+		private Transform leftOrigin; // The origin ("position") of the left controller.
 		private Vector3 leftVelocity; // How fast the left controller is moving in space.
+		private Vector3 leftAngularVelocity; // How fast the left controller is moving in space.
 
 		// True indicates that the button is being pressed, False that it's released.
 		private bool rightIndex;
@@ -30,36 +31,46 @@
 		private bool rightA;
 		private bool rightB;
 		private bool rightThumb;
-		private float rightThumbDeltaX; // The distance by which the tight stick has been moved in the X-axis.
-		private float rightThumbDeltaZ; // The distance by which the tight stick has been moved in the Z-axis.
-		private Vector3 rightPosition; // The position of the left controller.
-		private Vector3 rightVelocity; // How fast the left controller is moving in space.
-
+		private Vector2 rightThumbDelta; // The distance by which the right stick has been moved in the X-axis and Y-axis.
+		private float rightThumbAngle; // The angle of rotation of the right stick.
+		private Transform rightOrigin; // The origin ("position") of the right controller.
+		private Vector3 rightVelocity; // How fast the right controller is moving in space.
+		private Vector3 rightAngularVelocity; // How fast the right controller is moving in space.
 
 		// Update is called once per frame.
 		void Update () {
 
-			var right = RightController.GetComponent<VRTK_ControllerEvents>();
+			// These variables store the controller state, which can be queried for input information.
             var left = LeftController.GetComponent<VRTK_ControllerEvents>();
+			var right = RightController.GetComponent<VRTK_ControllerEvents>();
 
+			// Receive input information from the left controller.
 			leftIndex = left.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TriggerPress);
 			leftMiddle = left.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.GripPress);
 			leftX = left.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress);
 			leftY = left.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress);
 			leftThumb = left.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TouchpadPress);
+			leftThumbDelta = left.GetAxis(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
+			leftThumbAngle = left.GetAxisAngle(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
+			leftOrigin = VRTK_DeviceFinder.GetControllerOrigin(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
+			leftVelocity = VRTK_DeviceFinder.GetControllerVelocity(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
+			leftAngularVelocity = VRTK_DeviceFinder.GetControllerAngularVelocity(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
 
-			//float moveX = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
-            //float moveZ = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).y;
-            //float deltaX = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
-
+            // Receive input information from the right controller.
 			rightIndex = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TriggerPress);
 			rightMiddle = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.GripPress);
-			rightX = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress);
-			rightY = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress);
+			rightA = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonOnePress);
+			rightB = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.ButtonTwoPress);
 			rightThumb = right.IsButtonPressed(VRTK_ControllerEvents.ButtonAlias.TouchpadPress);
+			rightThumbDelta = right.GetAxis(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
+			rightThumbAngle = right.GetAxisAngle(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
+			rightOrigin = VRTK_DeviceFinder.GetControllerOrigin(VRTK_DeviceFinder.GetControllerReferenceRightHand());
+			rightVelocity = VRTK_DeviceFinder.GetControllerVelocity(VRTK_DeviceFinder.GetControllerReferenceRightHand());
+			rightAngularVelocity = VRTK_DeviceFinder.GetControllerAngularVelocity(VRTK_DeviceFinder.GetControllerReferenceRightHand());
 
+			// TODO: Add whether object is being grabbed. GetComponent<VRTK_InteractGrab>().GetGrabbedObject != null
+			// TODO: Add getter methods.
 
 		}
 	}
-
 }
