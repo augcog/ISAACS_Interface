4522308a566533e816168bf56de938962afe76e0
Apollo <apollo@apollo.vision>
2020-06-12 21:05:00 -0700

    Integrated Eric's review.

--- Assets/Scripts/Interaction Scripts/ControllerState.cs
+++ Assets/Scripts/Interaction Scripts/ControllerState.cs
@@ -2,42 +2,44 @@
 
     using System.Collections;
     using System.Collections.Generic;
-	using VRTK;
     using UnityEngine;
+		using VRTK;
+
+
+	/* This script is a wrapper around VRTK, to be used as a library for
+	higher-level scripts needing access to user input coming from a controller.
+	For further clarifications, ask Apollo.
+	*/
 
 	public class ControllerState : MonoBehaviour {
 
 		// Remember to set these controllers to actual GameObjects through the Unity GUI.
 		public GameObject LeftController;
 		public GameObject RightController;
-		public GameObject controller_left;
-		public GameObject controller_right;
 
-		// True indicates that the button is being pressed, False that it's released.
-		private bool LeftIsGrabbing; // Whether the left hand is grabbing another object.
-		private bool LeftIndex;
-		private bool LeftMiddle;
-		private bool LeftX;
-		private bool LeftY;
-		private bool LeftThumb;
-		private Vector2 LeftThumbDelta; // The distance by which the left stick has been moved in the X-axis and Y-axis.
-		private float LeftThumbAngle; // The angle of rotation of the left stick.
-		private Transform LeftOrigin; // The origin ("position") of the left controller.
-		private Vector3 LeftVelocity; // How fast the left controller is moving in space.
-		private Vector3 LeftAngularVelocity; // How fast the left controller is moving in space.
+		private bool LeftIsGrabbing; /// True if left hand is grabbing another object.
+		private bool LeftIndex; /// True if the left index trigger is being pressed.
+		private bool LeftMiddle; /// True if the left middle trigger is being pressed.
+		private bool LeftX; /// True if the left X button is being pressed.
+		private bool LeftY; /// True if the left Y button is being pressed.
+		private bool LeftThumb; /// True if the left thumbstick is being pressed.
+		private Vector2 LeftThumbDelta; /// The distance by which the left thumbstick has been moved in the X-axis and Y-axis.
+		private float LeftThumbAngle; /// The angle of rotation of the left thumbstick.
+		private Transform LeftOrigin; /// The origin ("position") of the left controller.
+		private Vector3 LeftVelocity; /// How fast the left controller is moving in space.
+		private Vector3 LeftAngularVelocity; /// How fast the left controller is rotating in space.
 
-		// True indicates that the button is being pressed, False that it's released.
-		private bool RightIsGrabbing; // Whether the right hand is grabbing another object.
-		private bool RightIndex;
-		private bool RightMiddle;
-		private bool RightA;
-		private bool RightB;
-		private bool RightThumb;
-		private Vector2 RightThumbDelta; // The distance by which the right stick has been moved in the X-axis and Y-axis.
-		private float RightThumbAngle; // The angle of rotation of the right stick.
-		private Transform RightOrigin; // The origin ("position") of the right controller.
-		private Vector3 RightVelocity; // How fast the right controller is moving in space.
-		private Vector3 RightAngularVelocity; // How fast the right controller is moving in space.
+		private bool RightIsGrabbing; /// True if the right hand is grabbing another object.
+		private bool RightIndex; /// True if the right index trigger is being pressed.
+		private bool RightMiddle; /// True if the right middle trigger is being pressed.
+		private bool RightA; /// True if the right A button is being pressed.
+		private bool RightB; /// True if the right B button is being pressed.
+		private bool RightThumb; /// True if the right thumbstick is being pressed.
+		private Vector2 RightThumbDelta; /// The distance by which the right thumbstick has been moved in the X-axis and Y-axis.
+		private float RightThumbAngle; /// The angle of rotation of the right thumbstick.
+		private Transform RightOrigin; /// The origin ("position") of the right controller.
+		private Vector3 RightVelocity; /// How fast the right controller is moving in space.
+		private Vector3 RightAngularVelocity; /// How fast the right controller is rotating in space.
 
 
 		// Update is called once per frame.
@@ -79,114 +81,114 @@
 
 
 		// Getter methods, left controller.
-		public bool GetLeftIsGrabbing()
+		public bool GetLeftIsGrabbing() /// True if left hand is grabbing an object.
 		{
 			return LeftIsGrabbing;
 		}
 		
-		public bool GetLeftIndex()
+		public bool GetLeftIndex() /// True if the left index trigger is being pressed.
 		{
 			return LeftIndex;
 		}
 
-		public bool GetLeftMiddle()
+		public bool GetLeftMiddle() /// True if the left middle trigger is being pressed.
 		{
 			return LeftMiddle;
 		}
 		
-		public bool GetLeftX()
+		public bool GetLeftX() /// True if the left X button is being pressed.
 		{
 			return LeftX;
 		}
 
-		public bool GetLeftY()
+		public bool GetLeftY() /// True if the left Y button is being pressed.
 		{
 			return LeftY;
 		}
 
-		public bool GetLeftThumb()
+		public bool GetLeftThumb() /// True if the left thumbstick is being pressed.
 		{
 			return LeftThumb;
 		}
 
-		public Vector2 GetLeftThumbDelta()
+		public Vector2 GetLeftThumbDelta() /// The distance by which the left thumbstick has been moved in the X-axis and Y-axis.
 		{
 			return LeftThumbDelta;
 		}
 
-		public float GetLeftThumbAngle()
+		public float GetLeftThumbAngle() /// The angle of rotation of the left thumbstick.
 		{
 			return LeftThumbAngle;
 		}
 
-		public Transform GetLeftOrigin()
+		public Transform GetLeftOrigin() /// The origin ("position") of the left controller.
 		{
 			return LeftOrigin;
 		}
 
-		public Vector3 GetLeftVelocity()
+		public Vector3 GetLeftVelocity() /// How fast the left controller is moving in space.
 		{
 			return LeftVelocity;
 		}
 
-		public Vector3 GetLeftAngularVelocity()
+		public Vector3 GetLeftAngularVelocity() /// How fast the left controller is rotating in space.
 		{
 			return LeftAngularVelocity;
 		}
 
 
 		// Getter methods, right controller.
-		public bool GetRightIsGrabbing()
+		public bool GetRightIsGrabbing() /// True if the right hand is grabbing an object.
 		{
 			return RightIsGrabbing;
 		}
 		
-		public bool GetRightIndex()
+		public bool GetRightIndex() /// True if the right index trigger is being pressed.
 		{
 			return RightIndex;
 		}
 
-		public bool GetRightMiddle()
+		public bool GetRightMiddle() /// True if the right middle trigger is being pressed.
 		{
 			return RightMiddle;
 		}
 		
-		public bool GetRightA()
+		public bool GetRightA() /// True if the right A button is being pressed.
 		{
 			return RightA;
 		}
 
-		public bool GetRightB()
+		public bool GetRightB() /// True if the right B button is being pressed.
 		{
 			return RightB;
 		}
 
-		public bool GetRightThumb()
+		public bool GetRightThumb() /// True if the right thumbstick is being pressed.
 		{
 			return RightThumb;
 		}
 
-		public Vector2 GetRightThumbDelta()
+		public Vector2 GetRightThumbDelta() /// The distance by which the right thumbstick has been moved in the X-axis and Y-axis.
 		{
 			return RightThumbDelta;
 		}
 
-		public float GetRightThumbAngle()
+		public float GetRightThumbAngle() /// The angle of rotation of the right thumbstick.
 		{
 			return RightThumbAngle;
 		}
 
-		public Transform GetRightOrigin()
+		public Transform GetRightOrigin() /// The origin ("position") of the right controller.
 		{
 			return RightOrigin;
 		}
 
-		public Vector3 GetRightVelocity()
+		public Vector3 GetRightVelocity() /// How fast the right controller is moving in space.
 		{
 			return RightVelocity;
 		}
 
-		public Vector3 GetRightAngularVelocity()
+		public Vector3 GetRightAngularVelocity() /// How fast the right controller is rotating in space.
 		{
 			return RightAngularVelocity;
 		}
