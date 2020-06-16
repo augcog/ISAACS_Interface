4ff79689f13edfc9267eb065791ce09f7e8a2362
Apollo <apollo@apollo.vision>
2020-06-15 20:17:41 -0700

    Controller State updated to accomodate for more complex states. Added position, localposition, rotation, localrotation, distance, localdistance, velocitydelta, angularvelocitydelta, dot, and localdot.

--- Assets/Scripts/Interaction Scripts/ControllerState.cs
+++ Assets/Scripts/Interaction Scripts/ControllerState.cs
@@ -17,6 +17,8 @@
 		public GameObject LeftController;
 		public GameObject RightController;
 
+
+		// These variables store information on the state of the left controller.
 		private bool LeftIsGrabbing; /// True if left hand is grabbing another object.
 		private bool LeftIndex; /// True if the left index trigger is being pressed.
 		private bool LeftMiddle; /// True if the left middle trigger is being pressed.
@@ -25,10 +27,16 @@
 		private bool LeftThumb; /// True if the left thumbstick is being pressed.
 		private Vector2 LeftThumbDelta; /// The distance by which the left thumbstick has been moved in the X-axis and Y-axis.
 		private float LeftThumbAngle; /// The angle of rotation of the left thumbstick.
-		private Transform LeftOrigin; /// The origin ("position") of the left controller.
+		private Transform LeftOrigin; /// The origin (position, rotation, scale) of the left controller.
+		private Vector3 LeftPosition; /// The position of the left controller.
+		private Vector3 LeftLocalPosition; /// The relative position of the left controller.
+		private Quaternion LeftRotation; /// The rotation of the left controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		private Quaternion LeftLocalRotation; /// The relative rotation of the left controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
 		private Vector3 LeftVelocity; /// How fast the left controller is moving in space.
 		private Vector3 LeftAngularVelocity; /// How fast the left controller is rotating in space.
 
+
+		// These variables store information on the state of the right controller.
 		private bool RightIsGrabbing; /// True if the right hand is grabbing another object.
 		private bool RightIndex; /// True if the right index trigger is being pressed.
 		private bool RightMiddle; /// True if the right middle trigger is being pressed.
@@ -37,13 +45,27 @@
 		private bool RightThumb; /// True if the right thumbstick is being pressed.
 		private Vector2 RightThumbDelta; /// The distance by which the right thumbstick has been moved in the X-axis and Y-axis.
 		private float RightThumbAngle; /// The angle of rotation of the right thumbstick.
-		private Transform RightOrigin; /// The origin ("position") of the right controller.
+		private Transform RightOrigin; /// The origin (position, rotation, scale) of the right controller.
+		private Vector3 RightPosition; /// The position of the right controller.
+		private Vector3 RightLocalPosition; /// The relative position of the right controller.
+		private Quaternion RightRotation; /// The rotation of the right controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		private Quaternion RightLocalRotation; /// The relative rotation of the right controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
 		private Vector3 RightVelocity; /// How fast the right controller is moving in space.
 		private Vector3 RightAngularVelocity; /// How fast the right controller is rotating in space.
 
 
-		// Update is called once per frame.
-		void Update () {
+		// These variables store information on the state of both controllers.
+		private bool BothMiddle; /// True if both the right and the left middle triggers are being pressed.
+		private Vector3 Distance; /// The absolute distance between the two controllers.
+		private Vector3 LocalDistance; /// The relative distance between the two controller. Unless the local scale is different from the absolute scale, LocalDistance == Distance.
+		private Vector3 VelocityDelta; /// The difference in velocity of the two controllers.
+		private Vector3 AngularVelocityDelta; /// The difference in angular velocity of the two controllers.
+		private float ScalingFactor; /// The dot product of VelocityDelta with Distance, indicating the absolute cartesian scaling factor.
+		private float LocalScalingFactor; /// The dot product of elocityDelta with LocalDistance, indicating the relative cartesian scaling factor.
+
+
+		// Unlike Update, which gets called once per frame, FixedUpdate is called accordingly to the physics engine.
+		void FixedUpdate() {
 
 			// These variables store the controller state, which can be queried for input information.
             var left = LeftController.GetComponent<VRTK_ControllerEvents>();
@@ -61,6 +83,10 @@
 			LeftThumbDelta = left.GetAxis(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
 			LeftThumbAngle = left.GetAxisAngle(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
 			LeftOrigin = VRTK_DeviceFinder.GetControllerOrigin(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
+			LeftPosition = LeftOrigin.position;
+			LeftLocalPosition = LeftOrigin.localPosition;
+			LeftRotation = LeftOrigin.rotation;
+			LeftLocalRotation = LeftOrigin.localRotation;
 			LeftVelocity = VRTK_DeviceFinder.GetControllerVelocity(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
 			LeftAngularVelocity = VRTK_DeviceFinder.GetControllerAngularVelocity(VRTK_DeviceFinder.GetControllerReferenceLeftHand());
 
@@ -74,9 +100,21 @@
 			RightThumbDelta = right.GetAxis(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
 			RightThumbAngle = right.GetAxisAngle(VRTK_ControllerEvents.Vector2AxisAlias.Touchpad);
 			RightOrigin = VRTK_DeviceFinder.GetControllerOrigin(VRTK_DeviceFinder.GetControllerReferenceRightHand());
+			RightPosition = RightOrigin.position;
+			RightLocalPosition = RightOrigin.localPosition;
+			RightRotation = RightOrigin.rotation;
+			RightLocalRotation = RightOrigin.localRotation;
 			RightVelocity = VRTK_DeviceFinder.GetControllerVelocity(VRTK_DeviceFinder.GetControllerReferenceRightHand());
 			RightAngularVelocity = VRTK_DeviceFinder.GetControllerAngularVelocity(VRTK_DeviceFinder.GetControllerReferenceRightHand());
 
+            // Compute information about the state of both controllers.
+			BothMiddle = LeftMiddle && RightMiddle;
+			Distance = LeftPosition - RightPosition;
+			LocalDistance = LeftLocalPosition - RightLocalPosition;
+			VelocityDelta = LeftVelocity - RightVelocity;
+			AngularVelocityDelta = LeftAngularVelocity - RightAngularVelocity;
+			ScalingFactor = Vector3.Dot(VelocityDelta, Distance);
+			LocalScalingFactor = Vector3.Dot(VelocityDelta, LocalDistance);
 		}
 
 
@@ -126,6 +164,26 @@
 			return LeftOrigin;
 		}
 
+		public Vector3 GetLeftPosition() /// The position of the left controller.
+		{
+			return LeftPosition;
+		}
+
+		public Vector3 GetLeftLocalPosition() /// The relative position of the left controller.
+		{
+			return LeftLocalPosition;
+		}
+
+		public Quaternion GetLeftRotation() /// The rotation of the left controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		{
+			return LeftRotation;
+		}
+
+		public Quaternion GetLeftLocalRotation() /// The relative rotation of the left controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		{
+			return LeftLocalRotation;
+		}
+
 		public Vector3 GetLeftVelocity() /// How fast the left controller is moving in space.
 		{
 			return LeftVelocity;
@@ -135,6 +193,8 @@
 		{
 			return LeftAngularVelocity;
 		}
+
+
 
 
 		// Getter methods, right controller.
@@ -183,6 +243,26 @@
 			return RightOrigin;
 		}
 
+		public Vector3 GetRightPosition() /// The position of the right controller.
+		{
+			return RightPosition;
+		}
+
+		public Vector3 GetRightLocalPosition() /// The relative position of the right controller.
+		{
+			return RightLocalPosition;
+		}
+
+		public Quaternion GetRightRotation() /// The rotation of the right controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		{
+			return RightRotation;
+		}
+
+		public Quaternion GetRightLocalRotation() /// The relative rotation of the right controller. Can be queried for its normalized value, eulerAngles, and w, x, y, and z components.
+		{
+			return RightLocalRotation;
+		}
+
 		public Vector3 GetRightVelocity() /// How fast the right controller is moving in space.
 		{
 			return RightVelocity;
@@ -193,5 +273,44 @@
 			return RightAngularVelocity;
 		}
 
+
+
+
+		// Getter methods, both controllers.
+		public bool GetBothMiddle() /// True if both the right and the left middle triggers are being pressed.
+		{
+			return BothMiddle;
+		}
+		
+		public Vector3 GetDistance() /// The absolute distance between the two controllers.
+		{
+			return Distance;
+		}
+		
+		public Vector3 GetLocalDistance() /// The relative distance between the two controller. Unless the local scale is different from the absolute scale, LocalDistance == Distance.
+		{
+			return LocalDistance;
+		}
+		
+		public Vector3 GetVelocityDelta() /// The difference in velocity of the two controllers.
+		{
+			return VelocityDelta;
+		}
+		
+		public Vector3 GetAngularVelocityDelta() /// The difference in angular velocity of the two controllers.
+		{
+			return AngularVelocityDelta;
+		}
+		
+		public float GetScalingFactor() /// The dot product of VelocityDelta with Distance, indicating the absolute cartesian scaling factor.
+		{
+			return ScalingFactor;
+		}
+		
+		public float GetLocalScalingFactor() /// The dot product of elocityDelta with LocalDistance, indicating the relative cartesian scaling factor.
+		{
+			return LocalScalingFactor;
+		}
+		
 	}
 }
