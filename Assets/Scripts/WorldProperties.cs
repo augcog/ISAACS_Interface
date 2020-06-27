45abe0ab15ff23ba3c0250e7f5c8f6d243f6b7b1
PeruDayani <perudayani@berkeley.edu>
2020-06-26 17:17:29 -0700

    ros <-> unity function definations, will continue in new breanch

--- Assets/Scripts/WorldProperties.cs
+++ Assets/Scripts/WorldProperties.cs
@@ -90,7 +90,32 @@
                 AddClipShader(child);
             }
         }
-        
+
+
+        // TODO: Complete these functions
+
+        /// <summary>
+        /// Convert the Given ROS NavSatFixMsg to Unity XYZ space.
+        /// To Be used to convert drone coordinates to unity space
+        /// </summary>
+        /// <returns></returns>
+        public static Vector3 ROSCoordToUnityCoord(NavSatFixMsg gpsPosition)
+        {
+            return Vector3.zero;
+        }
+
+        /// <summary>
+        /// Convert Unity coordinates to ROS world lat,long,alt
+        /// To be used to convert waypint unity coordinates to world lat.long.alt
+        /// </summary>
+        /// <param name="unityPosition"></param>
+        /// <returns></returns>
+        public static NavSatFixMsg UnityCoordToROSCoord(Vector3 unityPosition)
+        {
+            return null;
+        }
+
+
         /// <summary>
         ///     Converts the difference between two latitude values to a difference in meters.
         /// </summary>
@@ -121,17 +146,6 @@
             //slight inaccuracies
             double delLong = (long2 - long1) * 40075 * (double)Math.Cos(lat) / 360 * 1000;
             return delLong;
-        }
-
-        // TODO: Complete the function
-
-        /// <summary>
-        /// Convert the Given ROS NavSatFixMsg to Unity XYZ space.
-        /// </summary>
-        /// <returns></returns>
-        public static Vector3 ROSCoordToUnityCoord(NavSatFixMsg gpsPosition)
-        {
-            return Vector3.zero;
         }
 
         /// <summary>
