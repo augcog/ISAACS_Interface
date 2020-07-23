cae6f519701a45c3d855c0be787da590393e39ac
Jasmine Bae <jasmine.bae@berkeley.edu>
2020-07-22 19:22:27 -0700

    Finished camera streaming from /dji_sdk/fpv_camera_image topic

--- Assets/Scripts/ROS Scripts/Camera Streaming Scripts/ImageVisualizer.cs
+++ Assets/Scripts/ROS Scripts/Camera Streaming Scripts/ImageVisualizer.cs
@@ -2,43 +2,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageVisualizer : MonoBehaviour {
 
     //attached gameobject with texture2D that will change per frame
-    public GameObject imageTarget;
+    //GameObject s;
+    Texture2D originalTexture;
 
     string encoding;
     uint width;
     uint height;
     ImageMsg image;
+    RawImage m_RawImage;
 
-	// Use this for initialization
-	void Start () {
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
 	public void SetFrame(ImageMsg data)
     {
-        Texture2D thisTexture = new Texture2D((int) data.GetWidth(), (int) data.GetHeight());
-        Debug.Log(data.GetEncoding());
-        //encoding type is RGB8
-        byte[] pixelsR8 = data.GetImage();
-        Color32[] pixelsRGBA32 = new Color32[pixelsR8.Length];
-        for (int i = pixelsR8.Length - 1; i != -1; i--)
-        {
-            byte value = pixelsR8[i];
-            pixelsRGBA32[i] = new Color32(value, value, value, 255);//simplest R8 to RGBA32 conversion
-        }
-        thisTexture.SetPixels32(pixelsRGBA32);//updates textureRGBA32 data in CPU memory
-        thisTexture.Apply();//sends textureRGBA32 data from CPU memory to GPU (without this rendering wont change a bit)
-        imageTarget.GetComponent<Renderer>().material.mainTexture = thisTexture;
+        Texture2D tex = new Texture2D((int)data.GetWidth(), (int)data.GetHeight(), TextureFormat.RGB24, false);
+        GameObject.Find("CameraManager").GetComponent<RectTransform>().sizeDelta = new Vector2((int)data.GetWidth(), (int)data.GetHeight());
+        tex.LoadRawTextureData(data.GetImage());
+        tex.Apply();
+        GameObject.Find("CameraManager").GetComponent<RawImage>().texture = tex;
     }
 
-    
+
 }
