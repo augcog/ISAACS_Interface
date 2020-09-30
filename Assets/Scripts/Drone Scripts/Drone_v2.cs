8f782f9e15c4f8ecb5d87c96207804b7aba0cfcf
Huw-man <hu.newman@berkeley.edu>
2020-09-30 15:25:35 -0700

    setup drone_v2 skeleton

---
+++ Assets/Scripts/Drone Scripts/Drone_v2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone_v2 : Drone {
+    /// <summary>
+    /// Class for drone interactions with the server
+    /// Server calls are stateless so this class will hold drone state information
+    /// </summary>
+
+    private string _id;
+
+    public void uploadMission()
+    {
+        ServerConnection.uploadMission(this, _id, this.waypoints);
+    }
+
+    public void startMission()
+    {
+        ServerConnection.startMission(this, _id);
+    }
+
+    public void pauseMission()
+    {
+        ServerConnection.pauseMission(this, _id);
+    }
+
+    public void resumeMission()
+    {
+        ServerConnection.resumeMission(this, _id);
+    }
+
+    public void landDrone()
+    {
+        ServerConnection.landDrone(this, _id);
+    }
+
+    public void flyHome()
+    {
+        ServerConnection.flyHome(this, _id);
+    }
+
+    /******************************/
+    //    Callback Functions      //
+    /******************************/
+
+    public void onStartMission()
+    {
+
+    }
+
+    public void onPauseMission()
+    {
+
+    }
+
+    public void onResumeMission()
+    {
+
+    }
+
+    public void onLandDronoe()
+    {
+
+    }
+
+    public void onFlyHome()
+    {
+
+    }
+}
